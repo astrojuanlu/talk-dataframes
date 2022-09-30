e0951862fbe5e5e868496de0f055fbb0ff715fa2
Juan Luis Cano Rodríguez <hello@juanlu.space>
2022-09-30 14:17:15 +0200

    Update setup script

--- .orchest/environments/fa87a1fd-af2e-44a9-9ea2-3da408cc4b3a/setup_script.sh
+++ .orchest/environments/fa87a1fd-af2e-44a9-9ea2-3da408cc4b3a/setup_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt update && sudo -E apt install -y --no-install-recommends unzip
+sudo apt-get update && sudo -E apt-get install -y --no-install-recommends unzip
 
 mamba install -c conda-forge -y python-graphviz
 
